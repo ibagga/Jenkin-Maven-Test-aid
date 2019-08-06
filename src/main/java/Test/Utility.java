20ec544c255997d0a13d49242b62685fbfebece2
ibagga <ishu1070@gmail.com>
2019-08-06 11:53:26 +0530

    V4_6thAug

--- src/main/java/Test/Utility.java
+++ src/main/java/Test/Utility.java
@@ -1,5 +1,5 @@
 package Test;
-import java.io.File;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,34 +15,6 @@ public class Utility {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\i077219\\Desktop\\TrashBin\\chrome driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("http://localhost:8082/Jenkin-Maven-Web-aid/");  
-		String title = driver.getTitle();
-		System.out.print("Title is : " + title);
-		 	
-
-	}
-	@Test
-	public static void main1()
-	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\i077219\\Desktop\\TrashBin\\chrome driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("http://localhost:8082/Jenkin-Maven-Web-aid/");  
-		String title = driver.getTitle();
-		System.out.print("Title is : " + title);
-		 	
-
-	}
-	@Test
-	public static void main2()
-	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\i077219\\Desktop\\TrashBin\\chrome driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		
 		driver.get("http://localhost:8082/Jenkin-Maven-Web-aid/");  
 		String title = driver.getTitle();
 		System.out.print("Title is : " + title);
